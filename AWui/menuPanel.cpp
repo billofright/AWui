f346c58ab379ac1de1c6fc7468fa442dfa2080f1
billofright <bill.yuan27@gmail.com>
2022-07-07 15:16:57 -0700

    menu page corrections

--- AWui/menuPanel.cpp
+++ AWui/menuPanel.cpp
@@ -4,16 +4,16 @@ menuPanel::menuPanel(wxWindow* parent):wxPanel(parent)
 {
 	wxBoxSizer *boxSizer = new wxBoxSizer(wxVERTICAL);
 	
-	wxGridSizer *row1 = new wxGridSizer(1, 4, 10, 10);
-	wxGridSizer *row2 = new wxGridSizer(1, 4, 10, 10);
+	wxGridSizer *row1 = new wxGridSizer(1, 5, 10, 10);
+	wxGridSizer *row2 = new wxGridSizer(1, 5, 10, 10);
 	
-	btns1 = new wxButton*[4];
-	btns2 = new wxButton*[4];
+	btns1 = new wxButton*[5];
+	btns2 = new wxButton*[5];
 	
 	wxPanel *panel_middle = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
 	panel_middle -> SetBackgroundColour(wxColor(200, 200, 200));
 	
-	for(int i = 0; i < 4; i++){
+	for(int i = 0; i < 5; i++){
 		btns1[i] = new wxButton(this, 10001 + i, "", wxDefaultPosition, wxSize(0, 75), 0, wxDefaultValidator, wxButtonNameStr);
 		row1 -> Add(btns1[i], 0, wxEXPAND | wxALL);
 		
@@ -24,6 +24,13 @@ menuPanel::menuPanel(wxWindow* parent):wxPanel(parent)
 	
 	btns1[0] -> SetLabel("panel 1");
 	btns1[1] -> SetLabel("panel 2");
+	btns1[2] -> SetLabel("panel 3");
+	btns1[3] -> SetLabel("panel 4");
+	
+	btns2[0] -> SetLabel("panel 5");
+	btns2[1] -> SetLabel("panel 6");
+	btns2[2] -> SetLabel("panel 7");
+	btns2[3] -> SetLabel("panel 8");
 		
 	boxSizer -> Add(row1, 0, wxEXPAND | wxALL, 10);
 	boxSizer -> Add(panel_middle, 1, wxEXPAND | wxLEFT | wxRIGHT, 10);
