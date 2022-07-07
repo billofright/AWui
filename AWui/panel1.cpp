cf4262da89810b9baa05e125a86abefe3d249634
Andy Chen <andychen2121@gmail.com>
2022-07-07 16:05:40 -0700

    test2

--- AWui/panel1.cpp
+++ AWui/panel1.cpp
@@ -2,7 +2,7 @@
 
 panel1::panel1(wxWindow* parent):wxPanel(parent)
 {
-	text = new wxTextCtrl(this, wxID_ANY, "This is panel 1 wee woo wee woo", wxDefaultPosition, wxSize(640, 480), wxTAB_TRAVERSAL, wxDefaultValidator, wxTextCtrlNameStr);
+	text = new wxTextCtrl(this, wxID_ANY, "This is panel 1 wee woo wee woo woot weet", wxDefaultPosition, wxSize(640, 480), wxTAB_TRAVERSAL, wxDefaultValidator, wxTextCtrlNameStr);
 	text -> SetForegroundColour(wxColor(0, 0, 0));
 	this -> SetBackgroundColour(wxColor(255, 255, 0));
 	backButton = new wxButton(this, 10009, "back", wxDefaultPosition, wxSize(100, 100), 0, wxDefaultValidator, wxButtonNameStr);
